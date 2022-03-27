290bb5e7206222fc247c887fda8e88b32027e092
snsrrhc3078 <98512002+snsrrhc3078@users.noreply.github.com>
2022-03-27 22:40:59 +0900

    learn useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+// import Button from "./Button";
+// import styles from "./App.module.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1 className={styles.title}>Welcome back!</h1>
+//       <Button text="continue" />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((current) => current + 2);
+  };
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="continue" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
