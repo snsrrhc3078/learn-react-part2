833ccd4d2397bb220a20b89c997a3706374d0401
snsrrhc3078 <98512002+snsrrhc3078@users.noreply.github.com>
2022-04-01 21:54:59 +0900

    make movie information app + practice i've learned and learn React router

--- src/App.js
+++ src/App.js
@@ -1,70 +1,19 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [inputUsd, setInputUsd] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins((pre) => json);
-        setLoading(false);
-      });
-  }, []);
-  function onSubmit(event) {
-    event.preventDefault();
-    setInputUsd(false);
-    setUsd(event.target[0].value);
-    event.target[0].value = "";
-  }
-  function coinSelected(event) {
-    event.preventDefault();
-    setSelectedCoin((pre) => {
-      return [...pre, event.target[0].value];
-    });
-  }
-  function onClick(event) {
-    event.target.value = "";
-  }
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {inputUsd ? (
-        <form onSubmit={onSubmit}>
-          <input type="number" placeholder="input Your Budget"></input>
-        </form>
-      ) : loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <form onSubmit={coinSelected}>
-          <input
-            onClick={onClick}
-            type="text"
-            list="list"
-            placeholder="Search For coins"
-          ></input>
-        </form>
-      )}
-
-      <datalist id="list">
-        {coins.map((item) => (
-          <option>
-            {item.name} ({item.symbol}) : {item.quotes.USD.price.toFixed(2)}USD
-            You can buy {(usd / item.quotes.USD.price).toFixed(10)}
-            {item.symbol}
-          </option>
-        ))}
-      </datalist>
-
-      <ul>
-        {selectedCoin.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,28 @@
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title}></img>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        genres
+        {genres.map((genre, i) => (
+          <li key={i}>{genre}</li>
+        ))}
+      </ul>
+      <br></br>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string),
+};
+export default Movie;
