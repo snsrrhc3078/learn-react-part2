3fb5febf34896ceacb8fd6aa3b330ce2b9f80873
snsrrhc3078 <98512002+snsrrhc3078@users.noreply.github.com>
2022-04-04 03:18:50 +0900

    made Detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import propTypes from "prop-types";
 import { Link } from "react-router-dom";
-function Movie({ coverImg, title, summary, genres }) {
+
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title}></img>
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -20,6 +21,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: propTypes.number.isRequired,
   coverImg: propTypes.string.isRequired,
   title: propTypes.string.isRequired,
   summary: propTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import style from "../CSS/Detail.module.css";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [info, setInfo] = useState([]);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setInfo((current) => json.data.movie);
+  };
+  useEffect(getMovie, []);
+  document.body.style.backgroundImage = `url(${info.background_image_original})`;
+  return (
+    <div className={style.body}>
+      <img src={info.large_cover_image}></img>
+      <h1>{info.title_long}</h1>
+      <b>
+        Genres|
+        {info.genres &&
+          info.genres.map((item, i) => <span key={i}> {item}</span>)}
+      </b>
+      <p>{info.description_full}</p>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${info.yt_trailer_code}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 }
 export default Detail;
